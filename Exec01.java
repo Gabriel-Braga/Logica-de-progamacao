29ee3943b6dff8d74d77624894ccab15c6d2bd6f
Gabriel Braga <fetalcape@gmail.com>
2018-08-21 20:59:29 -0300

    new

--- Exec01.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Exec01 {
-    public static void main (String args[]) {
-        Scanner tcl = new Scanner(System.in);
-        int a = tcl.nextInt();
-        int b = tcl.nextInt();
-        System.out.println("A soma dos números digitados é : "+ (a + b));
-    }
-}
